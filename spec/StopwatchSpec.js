d073e329225077d26c8843f7504625286051b972
marisid <sid_marina@hotmail.com>
2016-10-26 18:30:05 +0100

    Pass basic tests and implement millisecond counting

--- spec/StopwatchSpec.js
+++ spec/StopwatchSpec.js
@@ -1,5 +1,36 @@
-describe("Basic test", function() {
-  it('should be able to get time', function() {
-    var actual;
-  })
+describe("Basic tests when starting the stopwatch", function() {
+    var clickSpy = jasmine.createSpy();
+  it('should be set to paused on load', function() {
+    expect(isPaused).toBe(true);
+  });
+
+  // it('should start on clicking the start button', function() {
+  //   expect(clickSpy).toHaveBeenCalled();
+  //   expect(isPaused).toBe(false);
+  // })
+
+  it('should be able to start working', function() {
+    stopwatch.startTimer();
+    expect(isPaused).toBe(false);
+  });
+
+  it('should be able to stop working', function() {
+    stopwatch.stopTimer();
+    expect(isPaused).toBe(true);
+  });
+
+  it('should be able to pause on clicking reset', function() {
+    stopwatch.resetTimer();
+    expect(isPaused).toBe(true);
+  });
+
+  it('should be able to get a starting time', function() {
+    stopwatch.startTimer();
+    expect(startingTime).toEqual(new Date());
+  });
+
+  // it('should be able to count',function() {
+  //
+  // })
+
 })
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,3 +1,49 @@
-function stopwatch() {
-  
+window.onload(function(){
+  isPaused = true;
+});
+
+var isPaused = true;
+var ms = document.getElementById("milliseconds");
+var startingTime, currentTime, milliseconds;
+console.log(document, ms);
+
+var stopwatch = {
+  startTimer: function() {
+    isPaused = false;
+    startingTime = startingTime || new Date();
+  },
+  stopTimer: function() {
+    isPaused = true;
+    startingTime = currentTime;
+  },
+  resetTimer: function() {
+    isPaused = true;
+    startingTime = null;
+    ms.innerHTML = 000;
+  }
 }
+
+setInterval(function(){
+  console.log('setInterval isPaused = ' + isPaused);
+  if(!isPaused) {
+    currentTime = new Date();
+    milliseconds = currentTime - startingTime;
+    ms.innerHTML = milliseconds;
+  }
+},1000);
+
+document.getElementById('pause').addEventListener('click', function(e){
+  e.preventDefault();
+  stopwatch.stopTimer();
+  console.log('setInterval isPaused = ' + isPaused);
+})
+
+document.getElementById('start').addEventListener('click', function(e) {
+  e.preventDefault();
+  stopwatch.startTimer();
+});
+
+document.getElementById('reset').addEventListener('click', function(e) {
+  e.preventDefault();
+  stopwatch.resetTimer();
+})

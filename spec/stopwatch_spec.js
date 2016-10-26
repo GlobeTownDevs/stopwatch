ea2ed3c799db73e36c1b3516883a32baa8e76142
Nick Field <heyimnick@live.com>
2016-10-26 12:23:42 +0100

    first test added & passed

--- spec/stopwatch_spec.js
+++ spec/stopwatch_spec.js
@@ -1,1 +1,8 @@
 /* holds all jasmine test */
+describe("Stopwatch", function() {
+  it("Adds text contact into html (into div with id 'stopwatch')", function() {
+    var stopwatch = new Stopwatch('stopwatch');
+    expect(document.getElementById('stopwatch').textContent).toBe('hello');
+  });
+
+ });
--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,1 +1,7 @@
 /* holds stopwatch function */
+function Stopwatch(id){
+    this.id = id;
+
+    var elt = document.getElementById(id);
+    elt.textContent = "hello";
+}
